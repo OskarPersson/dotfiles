b8ad1c525acd7f8700b0a60fba184d05b4c4a7ef
Oskar Persson <oskar.persson@polken.se>
2016-06-19 20:12:25 +0200

    Minor changes to linux install script

--- linuxinstall.sh
+++ linuxinstall.sh
@@ -1,10 +1,8 @@
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install neovim python-dev python-pip python3-dev python3-pip zsh dconf-cli
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp ~/git/dotfiles/zshrc ~/.zshrc
+sudo apt-get install neovim python-dev python-pip python3-dev python3-pip zsh dconf-cli xclip
+chsh -s $(which zsh)
 
 git clone https://github.com/metalelf0/gnome-terminal-colors.git ~/git/gnome-terminal-colors
 ~/git/gnome-terminal-colors/install.sh
@@ -12,3 +10,6 @@ git clone https://github.com/metalelf0/gnome-terminal-colors.git ~/git/gnome-ter
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ~/git/dotfiles/nvim.vim ~/.config/nvim/init.vim
 nvim -c PlugInstall
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp ~/git/dotfiles/zshrc ~/.zshrc
