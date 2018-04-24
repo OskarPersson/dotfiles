625e0f8441d6bda29237074040a39d1ae131e373
Oskar Persson <oskar.persson@polken.se>
2018-04-25 00:14:14 +0200

    Update homebrew packages

--- brew.sh
+++ brew.sh
@@ -8,7 +8,11 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #Packages
-brew install emacs
+brew install asciinema
 brew install ffmpeg
-brew install node
+brew install htop
+brew install pyenv
+brew install pyenv-virtualenv
+brew install ripgrep
+brew install vim
 brew install youtube-dl
